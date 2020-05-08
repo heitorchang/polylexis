999a19617c621028249ad17bde74a66718c13afe
Heitor Chang <heitorchang@gmail.com>
2020-05-08 12:44:58 -0300

    copy italian phrasebook build

---
+++ ui/static/italian/precache-manifest.e855012daee7844f545fb6594c8c6715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e3f1e5b3895d1c79e37d0b654efeb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2c841fa27bd5aa416aa",
+    "url": "/static/css/main.61bf12bd.chunk.css"
+  },
+  {
+    "revision": "aca4f900bb7590db6bb0",
+    "url": "/static/js/2.45ea6b2c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.45ea6b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2c841fa27bd5aa416aa",
+    "url": "/static/js/main.c663388b.chunk.js"
+  },
+  {
+    "revision": "3697de50a11d90d738ae",
+    "url": "/static/js/runtime-main.92309a1b.js"
+  }
+]);
